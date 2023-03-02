3431ce37f4f66573df65448c41a27b96fa5da367
F.Garcia <fagarcia93@gmail.com>
2023-03-02 12:01:07 -0500

    Adding missing path

--- StormSim_Library/Visualization/plot_structure_geometry.m
+++ StormSim_Library/Visualization/plot_structure_geometry.m
@@ -41,6 +41,8 @@ function [fig] = call_structure_geometry_plotter(config, structure)
     structure_id = config.struc_id;
     % Define Structure Type
     structure_type = config.struc_type;
+    % Define Case Name 
+    case_name = config.case_name;
     
     %% DEFINE FIGURE PROPERTIES
     % Figure Title
@@ -149,5 +151,5 @@ function [fig] = call_structure_geometry_plotter(config, structure)
     plot(ax,sand_geo,'FaceColor',[0.9 0.72 0.31],'FaceAlpha',1);
     
     %% EXPORT FIGURE 
-    saveas(fig, [project_name filesep structure_id filesep project_name '_' structure_id '_structure_cross_section.png']);
+    saveas(fig, [project_name filesep structure_id filesep case_name filesep project_name '_' structure_id '_structure_cross_section.png']);
 end
