af24ecb07cc5ed3ed2728f3015db3bd71fa84567
F.Garcia <fagarcia93@gmail.com>
2023-12-14 14:59:29 -0600

    Cleaning up relic comments

--- StormSim_Library/Forcing_Generation/call_project_forcing_adjuster.m
+++ StormSim_Library/Forcing_Generation/call_project_forcing_adjuster.m
@@ -2,50 +2,54 @@ function [project_forcing, config] = call_project_forcing_adjuster(config, proje
 %% GRAB INPUTS FROM "config"
 % Define Workflow
 workflow = config.workflow;
+% Define If Peaks Are Being Used 
 use_peaks = config.use_peaks;
+% Grab Forcing Adjsutment Flag 
 f_adjust = config.f_adjust;
 
-%% COMPUTE STRUCTURE RESPONSE BASED ON WORKFLOW
+%% ADJUST PROJECT FORCING ACCORDING TO WORKFLOW
+% Adjust Forcing If Needed
 if f_adjust == 0
-disp('Adjusting project forcing datasets....')
-% Determine Data Type
-switch workflow
-    case {1,2,4}
-        wName = 'RB';
-    case 3
-        wName = 'LCS';
-end
-% Grab "project_forcing" Structure Fields
-level_1 = fieldnames(project_forcing);
-% Scan Peaks Datasets
-if use_peaks == 1
-    level_2 = fieldnames(project_forcing.(level_1{1}).('Peaks'));
-    level_2 = level_2(contains(level_2,{'Maxima','WLP','WHP'}));
-end
-% Scan Peaks And Timeseries Field
-level_3 = fieldnames(project_forcing.(level_1{1}));
-level_3 = level_3(contains(level_3,{'Peaks','Timeseries'}));
-% Loop Through All Peak Datasets & Storm Types
-for jj = 1:length(level_1)
-    % Loop Through Peaks/Timeseries
-    for kk = 1:length(level_3)
-        if strcmp(level_3{kk},{'Peaks'})
-            for ii = 1:length(level_2)
+    % Print Status
+    disp('Adjusting project forcing datasets....');
+    % Determine Data Type
+    switch workflow
+        case {1,2,4} % StormSim: PROS
+            wName = 'PROS';
+        case 3 % StormSim: LCS
+            wName = 'LCS';
+    end
+    % Grab "project_forcing" Structure Fields
+    level_1 = fieldnames(project_forcing);
+    % Scan Peaks Datasets
+    if use_peaks == 1
+        level_2 = fieldnames(project_forcing.(level_1{1}).('Peaks'));
+        level_2 = level_2(contains(level_2,{'Maxima','WLP','WHP'}));
+    end
+    % Loop Through All Peak Datasets & Storm Types
+    for jj = 1:length(level_1)
+        % Scan Peaks And Timeseries Field
+        level_3 = fieldnames(project_forcing.(level_1{jj}));
+        level_3 = level_3(contains(level_3,{'Peaks','Timeseries'}));
+        % Loop Through Peaks/Timeseries
+        for kk = 1:length(level_3)
+            if strcmp(level_3{kk},{'Peaks'})
+                for ii = 1:length(level_2)
+                    % Create Aux Var
+                    aux_var = project_forcing.(level_1{jj}).('Peaks').(level_2{ii});
+                    % Assign To Output Variable
+                    project_forcing.(level_1{jj}).('Peaks').(level_2{ii}) = adjust_project_forcing(config, structure, aux_var, level_1{jj}, wName);
+                end
+            else % Timeseries
                 % Create Aux Var
-                aux_var = project_forcing.(level_1{jj}).('Peaks').(level_2{ii});
-                %
-                project_forcing.(level_1{jj}).('Peaks').(level_2{ii}) = adjust_project_forcing(config, structure, aux_var, level_1{jj}, wName);
-                %
+                aux_var = project_forcing.(level_1{jj}).('Timeseries');
+                % Assign To Output Variable
+                project_forcing.(level_1{jj}).('Timeseries') = adjust_project_forcing(config, structure, aux_var, level_1{jj}, wName);
             end
-        else
-            % Create Aux Var
-            aux_var = project_forcing.(level_1{jj}).('Timeseries');
-            %
-            project_forcing.(level_1{jj}).('Timeseries') = adjust_project_forcing(config, structure, aux_var, level_1{jj}, wName);
         end
     end
-end
-config.f_adjust = 1;
+    % Raise Flag In Case Function Is Executed Again
+    config.f_adjust = 1;
 else
     % Display Status Message
     disp('Project forcing has already been adjusted, skipping....');
