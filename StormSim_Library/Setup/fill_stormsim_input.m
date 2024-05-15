c4042848c30ab2443212e7a29c8f4a92b9fb839c
F.Garcia <fagarcia93@gmail.com>
2024-05-15 14:02:26 -0500

    Switch script logic to keep color coding formatting.

--- StormSim_Library/Setup/fill_stormsim_input.m
+++ StormSim_Library/Setup/fill_stormsim_input.m
@@ -1,8 +1,17 @@
 function fout = fill_stormsim_input(ifile, outpath, fields_2_change, values_2_change)
+% Get Sotrage Folder
+[fpath, ~, ~] = fileparts(outpath);
+% Create Output Dir If Needed
+if ~exist(fpath, 'dir')
+    mkdir(fpath);
+end
+% Copy Template
+copyfile(ifile, outpath);
 % Read Input File Template
-input_template = readcell(ifile);
+input_template = readcell(outpath);
 % Find Rows
 for kk = 1:length(fields_2_change)
+    % Find Entry
     [rr,cc] = find(strcmp(input_template,fields_2_change{kk}));
     input_template{rr, cc+3} = values_2_change{kk};
 end
