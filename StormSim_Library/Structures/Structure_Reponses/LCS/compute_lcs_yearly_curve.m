22ce53693cc2a392255fa16a861917ea5071e7fc
F.Garcia <fagarcia93@gmail.com>
2024-06-28 16:28:02 -0500

    Fixed yearly maximum binning to account for multiple storms at the same year, LC.

--- StormSim_Library/Structures/Structure_Reponses/LCS/compute_lcs_yearly_curve.m
+++ StormSim_Library/Structures/Structure_Reponses/LCS/compute_lcs_yearly_curve.m
@@ -1,9 +1,9 @@
-function [Smax,SPcurves] = compute_lcs_yearly_curve(data,resp_vec,nYears)
+function [Smax,SPcurves,yBox,dbug] = compute_lcs_yearly_curve(data,resp_vec,nYears, dbug_type)
 
 
 % Define Years To Look For
 years = [1:nYears];yBox = num2cell(zeros(size(years))');
-dbug = num2cell(zeros(length(years),length(data)));
+dbug = num2cell(nan(length(years),length(data)));
 % Define Col Indexes For Peaks & Timeseries
 switch size(data{1},2)
     case 10 % Timeseries
@@ -24,25 +24,6 @@ for jj = 1:length(years)
     for ii = 1:length(data)
         % Extract Storm Data From Life Cycle
         lc_data = data{ii};
-        % Find Start Row of Each Concatenated Storm
-        if exist('indx3','var') == 1 % Timeseries
-            % Find Start Row of Each Concatenated Storm
-            start_indx = find(lc_data(:,indx1)==0);
-            % Extract Data Length Col
-            dlen = lc_data(start_indx,indx3);
-        else
-            % Find Start Row of Each Concatenated Storm
-            start_indx = [1:length(data)]';
-            % Extract Data Length Col
-            dlen = 0;
-        end
-        % Extract Strom Year Col
-        dyear = lc_data(start_indx,indx2);
-        % Get Indexes Of Storms Ocurring on Years(jj)
-        yIndx = find(dyear==years(jj));
-        dummy3 = start_indx(yIndx);
-        % Get Length Of Storms Matching Years(jj)
-        dlen = dlen(yIndx);
         % Get Storm Hydrograph Delimiters
         stm_beg = find(lc_data(:,indx1)==0);
         % Extract Strom Year Col
@@ -56,17 +37,31 @@ for jj = 1:length(years)
         dlen = dlen(wanted_years);
         % Cycle Throuigh Matching Storms
         if isempty(wanted_stm_beg)==0
-            % Seaside
+            % Grab All Storm Data Associated To Specific Year
             yData = resp_vec{ii}(wanted_stm_beg(1):wanted_stm_beg(end)+dlen(end)-1);
-            dbug{jj,ii} = yData;
-            if ii==1
-                yBox{jj} = max(yData);
+            if dbug_type == 1
+                dbug{jj,ii} = [repmat(ii,length([wanted_stm_beg(1):wanted_stm_beg(end)+dlen(end)-1]),1), lc_data(wanted_stm_beg(1):wanted_stm_beg(end)+dlen(end)-1,:)];
             else
-                yBox{jj} = [yBox{jj};max(yData)];
+                dbug{jj,ii} = yData;%wanted_stm_beg(1):wanted_stm_beg(end)+dlen(end)-1;
+            end
+            % Check If We Have Multiple Storms 
+            if length(dlen)>1
+                % Find Individual Storm Max Response 
+                yData = arrayfun(@(x,y) max(resp_vec{ii}(x:x+y-1)),wanted_stm_beg, dlen, 'un', true);
+            else
+                yData = max(yData);
+            end
+            if ii==1
+                yBox{jj} = yData;
+            else
+                yBox{jj} = [yBox{jj};yData];
             end
         end
     end
 end
+%
+% yBox = mean(cell2mat(cellfun(@(x) max(x), dbug, 'un', false)), 2, 'omitnan');
+%
 SPcurves = cell2mat(cellfun(@(x) prctile(x,[10,16,84,90]),yBox,'un',false));
 Smax = cell2mat(cellfun(@(x) mean(x,'omitnan'),yBox,'un',false));
 end
