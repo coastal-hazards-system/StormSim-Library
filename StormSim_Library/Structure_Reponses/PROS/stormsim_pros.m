53233191ad1a1665b1c81c7b1685906a30867a76
FGM <fagarcia93@@gmail.com>
2026-07-21 09:44:31 -0400

    Added no_reps replacement logic to pros call.

--- StormSim_Library/Structure_Reponses/PROS/stormsim_pros.m
+++ StormSim_Library/Structure_Reponses/PROS/stormsim_pros.m
@@ -4,6 +4,8 @@ function HC_out = stormsim_pros(config, project_forcing, structure, emp_coeff, o
 warning('off');
 % Get AEP vs AEF Flag
 use_aep = config.pros_use_aep;
+% Force LCBW FS To False, Not supported on PROS 
+% config.compute_dn50_lcbw = 0;
 % Display PROS Message
 disp(['      Running StormSim: PROS....']);
 % Get Secondary Responses Switch
@@ -34,6 +36,8 @@ end
 if contains(config.storm_sampling, {'XC','CC'})
     % 1. Compute Structure Responses
     [Resp] = compute_structure_response(config, structure, project_forcing.('XC'), emp_coeff, 1);
+    % 1.1 Overwrite Storm Responses with no_rep
+    Resp = add_forcing(project_forcing, 'XC', Resp);
     % 2. Compute Extremal Distribution Curves
     HC_out.XC = call_hazard_curve_builder(config, [], Resp, 'XC', use_aep, outPath);
 end
@@ -42,6 +46,8 @@ end
 if contains(config.storm_sampling, {'TC','CC'})
     % 1. Compute Structure Responses
     [Resp] = compute_structure_response(config, structure, project_forcing.('TC'), emp_coeff, 1);
+    % 1.1 Overwrite Storm Responses with no_rep
+    Resp = add_forcing(project_forcing, 'TC', Resp);
     % 2. Compute Extremal Distribution Curves
     HC_out.TC = call_hazard_curve_builder(config, project_forcing.TC.TC_Prob, Resp, 'TC', use_aep, outPath);
 end
@@ -83,6 +89,21 @@ warning('on');
         % Turn Off Responses For RB Section
         for ii = 1:length(var_to_look)
             config_out.(var_to_look{ii}) = value;
+        end
+    end
+
+    function Resp = add_forcing(project_forcing, st_type, Resp)
+        if  config.pros_compute_forcing_HC == 1 & any(contains(fieldnames(project_forcing.(st_type)),{'_no_rep'}))
+            % Grab Values With No Uncertainty For HC Calculations
+            SWL = project_forcing.(st_type).('SWL_no_rep');
+            Hm0 = project_forcing.(st_type).('Hm0_no_rep');
+            Tp = project_forcing.(st_type).('Tp_no_rep');
+            % This Max Is Mostly Meant To Handle Hm0/Tp Max Relationship
+            Resp.('SWL') = cellfun(@(x) max(x,[],1),SWL,'un',true);
+            % Find Hm0 Max For Each Storm
+            [Resp.('Hm0'), Hm0_indx] = cellfun(@(x) max(x,[],1),Hm0,'un',true);
+            %
+            Resp.('Tp') = cellfun(@(x,y) x(y), Tp, num2cell(Hm0_indx), 'un', true);
         end
     end
 end
