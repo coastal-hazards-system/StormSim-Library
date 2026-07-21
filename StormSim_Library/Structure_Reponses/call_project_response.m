99084da1ab4fc205f1a968904861059d6241f32c
FGM <fagarcia93@@gmail.com>
2026-07-21 10:01:15 -0400

    Consolidating logic.

--- StormSim_Library/Structure_Reponses/call_project_response.m
+++ StormSim_Library/Structure_Reponses/call_project_response.m
@@ -1,121 +1,116 @@
 function [Resp] = call_project_response(config, project_forcing, structure, create_plots)
-%% GRAB INPUTS FROM "config"
-% Define Workflow
-workflow = config.workflow;
-% Define Storm Sampling
-storm_sampling = config.storm_sampling;
-%
-use_peaks = config.use_peaks;
-use_timeseries = config.use_timeseries;
-% WLP & WHP Switches
-use_whp = config.create_whp;
-use_wlp = config.create_wlp;
-% Project Name
-project_name = config.project_name;
-% Transect Id
-struc_id = config.struc_id;
-% Define Case  Name
-case_name = config.case_name;
-% Define
-subDir = fullfile(config.outfolder, project_name, struc_id, case_name);
-% Load Empirical Coefficients
-[emp_coeff] = load_empirical_coefficients();
-% Intialize Output Data Strucuture
-Resp = struct();
+% CALL_PROJECT_RESPONSE Computes structure response based on a workflow config.
 
-%% COMPUTE STRUCTURE RESPONSE BASED ON WORKFLOW
-switch workflow
-    case {1,2,4} % PROS
-        %% STORMSIM: PROS
-        % Define Workflow ID String
-        if workflow == 4
-            % Define Workflow Name
-            wName = 'FB';
-            subDir = [fullfile(subDir, 'PROS'), filesep];
-        else
-            % Define Workflow Name
-            wName = 'RB';
-            subDir = [fullfile(subDir, 'PROS'), filesep];
-        end
-        % Make Dir
-        if ~exist(subDir ,'dir')
-            mkdir(subDir);
-        end
-        % Process Peaks Datasets: RB1
-        if use_peaks == 1
-            % Scan Peaks Datasets
-            level_2 = fieldnames(project_forcing.('Peaks')); % Matching Type
-            % Disp
-            disp('Running StormSim: Probabilistic Response Of Structures (PROS) - Peaks....');
-            % Loop Through All Peak Datasets & Storm Types
-            for ii = 1:length(level_2)
-                % Skip Based On Config File
-                if (strcmp(level_2{ii},'WLP') && use_wlp == 0) || (strcmp(level_2{ii},'WHP') && use_whp == 0)
-                    continue;
-                else % Pass Peaks Dataset Through StormSim: PROS
-                    % Disp
-                    disp(['   Processing ' level_2{ii} ' dataset....']);
-                    % Call StormSim: PROS RB1
-                    Resp.('Peaks').(level_2{ii}) = stormsim_pros(config, project_forcing.('Peaks').(level_2{ii}),...
-                        structure, emp_coeff, fullfile(subDir, [ wName '1_' level_2{ii} ]));
+    %% ================= CONFIGURATION & VARIABLE MAPPING =================
+    % Modify these variables to change how fields are read from 'config'
+    workflow       = config.workflow;
+    storm_sampling = config.storm_sampling;
+    use_peaks      = config.use_peaks;
+    use_timeseries = config.use_timeseries;
+    use_whp        = config.create_whp;
+    use_wlp        = config.create_wlp;
+    project_name   = config.project_name;
+    struc_id       = config.struc_id;
+    case_name      = config.case_name;
+    
+    % Core Output File Target
+    save_target    = config.out_files.resp_data;
+    % =====================================================================
+
+    % Load Empirical Coefficients & Initialize Output Struct
+    [emp_coeff] = load_empirical_coefficients();
+    Resp        = struct();
+    
+    % Establish Base Output Directory 
+    subDir = fullfile(config.outfolder, project_name, struc_id, case_name);
+
+    %% COMPUTE STRUCTURE RESPONSE BASED ON WORKFLOW
+    switch workflow
+        
+        case {1, 2, 4} % ================= PROS WORKFLOW =================
+            % Define workflow parameters
+            wName  = ifthen(workflow == 4, 'FB', 'RB');
+            subDir = fullfile(subDir, 'PROS');
+            ensure_directory(subDir);
+
+            % Process Peaks Datasets (PROS)
+            if use_peaks && isfield(project_forcing, 'Peaks')
+                disp('Running StormSim: Probabilistic Response Of Structures (PROS) - Peaks....');
+                
+                peak_fields = fieldnames(project_forcing.Peaks);
+                for i = 1:numel(peak_fields)
+                    fName = peak_fields{i};
+                    
+                    % Filter based on water level/height configurations
+                    if (strcmp(fName, 'WLP') && ~use_wlp) || (strcmp(fName, 'WHP') && ~use_whp)
+                        continue; 
+                    end
+                    
+                    fprintf('   Processing %s dataset....\n', fName);
+                    save_path = fullfile(subDir, sprintf('%s1_%s', wName, fName));
+                    
+                    Resp.Peaks.(fName) = stormsim_pros(config, ...
+                        project_forcing.Peaks.(fName), structure, emp_coeff, save_path);
                 end
             end
-        end
-        % RB3
-        if use_timeseries == 1
-            % Disp
-            disp('Running StormSim: Probabilistic Response Of Structures (PROS) - Timeseries....');
-            % Call StormSim: PROS RB3
-            Resp.('Timeseries').('Default') = stormsim_pros(config, project_forcing.('Timeseries').('Default'),...
-                structure, emp_coeff, fullfile(subDir, [ wName '3' ]));
-        end
-        % Plot Results
-        if create_plots == 1
-            disp('      Plotting StormSim: PROS Analysis outputs... ');
-            call_pros_plots(config, structure, project_forcing, Resp);
-        else
-            if ~exist(subDir ,'dir')
-                mkdir(subDir);
+
+            % Process Timeseries Datasets (PROS)
+            if use_timeseries && isfield(project_forcing, 'Timeseries')
+                disp('Running StormSim: Probabilistic Response Of Structures (PROS) - Timeseries....');
+                save_path = fullfile(subDir, [wName, '3']);
+                
+                Resp.Timeseries.Default = stormsim_pros(config, ...
+                    project_forcing.Timeseries.Default, structure, emp_coeff, save_path);
             end
-        end
-        % Update Naming Label 
-        wName = ['PROS-' wName];
-    case 3 % CSR
-        %% STORMSIM: LCS-CSR
-        % Define Workflow ID
-        wName = 'LCS';
-        % Define Workflow Subdirectory
-        subDir = [subDir filesep 'LCS' filesep];
-        % Create Subdirectory
-        if ~exist(subDir,'dir')
-            mkdir(subDir);
-        end
-        % Scan Peaks Datasets
-        if use_peaks == 1
-            % Print Status
-            disp('Running StormSim: Coastal Structure Reliability - Peaks....');
-            %
-            level_2 = fieldnames(project_forcing.Peaks);
-            % Loop Through All Peak Datasets
-            for ii = 1:length(level_2)
-                disp(['   Processing ' level_2{ii} ' dataset....']);
-                Resp.('Peaks').(level_2{ii}) = ...
-                    stormsim_csr(config, project_forcing.('Peaks').(level_2{ii}),...
-                    structure, emp_coeff, 'peaks', subDir);
+
+            % Plotting Outputs
+            if create_plots
+                disp('      Plotting StormSim: PROS Analysis outputs... ');
+                call_pros_plots(config, structure, project_forcing, Resp);
             end
-        end
-        % Timeseries
-        if use_timeseries == 1
-            disp('Running StormSim: Coastal Structure Reliability - Timeseries....');
-            Resp.('Timeseries').('Default') = ...
-                stormsim_csr(config, project_forcing.('Timeseries').('Default'),...
-                structure, emp_coeff, 'timeseries', subDir);
-        end
+
+        case 3 % ================== CSR WORKFLOW ==================
+            % Define workflow parameters
+            subDir = fullfile(subDir, 'LCS');
+            ensure_directory(subDir);
+
+            % Process Peaks Datasets (CSR)
+            if use_peaks && isfield(project_forcing, 'Peaks')
+                disp('Running StormSim: Coastal Structure Reliability - Peaks....');
+                
+                peak_fields = fieldnames(project_forcing.Peaks);
+                for i = 1:numel(peak_fields)
+                    fName = peak_fields{i};
+                    fprintf('   Processing %s dataset....\n', fName);
+                    
+                    Resp.Peaks.(fName) = stormsim_csr(config, ...
+                        project_forcing.Peaks.(fName), structure, emp_coeff, 'peaks', subDir);
+                end
+            end
+
+            % Process Timeseries Datasets (CSR)
+            if use_timeseries && isfield(project_forcing, 'Timeseries')
+                disp('Running StormSim: Coastal Structure Reliability - Timeseries....');
+                
+                Resp.Timeseries.Default = stormsim_csr(config, ...
+                    project_forcing.Timeseries.Default, structure, emp_coeff, 'timeseries', subDir);
+            end
+    end
+
+    %% EXPORT OUTPUTS
+    disp('Saving project responses....');
+    save(save_target, 'Resp', '-v7.3');
 end
 
-%% EXPORT OUTPUTS
-% Display Status
-disp('Saving project responses....');
-% Save Outputs
-save(config.out_files.resp_data,'Resp','-v7.3');
+%% ================= HELPER FUNCTIONS =================
+function ensure_directory(dir_path)
+    % Verifies and creates directory if it doesn't exist
+    if ~exist(dir_path, 'dir')
+        mkdir(dir_path);
+    end
+end
+
+function out = ifthen(cond, true_val, false_val)
+    % Fast inline ternary condition
+    if cond, out = true_val; else, out = false_val; end
 end
